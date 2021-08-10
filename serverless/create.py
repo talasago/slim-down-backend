3eff6a0b15a1fef060f76b5ce90bdf6e4e68e46a
talasago <lyricaldragons@gmail.com>
2021-08-10 22:20:11 +0900

    feat: slsofflineを構築

--- serverless/create.py
+++ serverless/create.py
@@ -1,27 +1,37 @@
 import json
 import logging
 import os
-import time
-import uuid
+import datetime
 
 import boto3
 
 dynamodb = boto3.resource('dynamodb')
 
+if os.environ['IS_OFFLINE']:
+    dynamodb = boto3.resource('dynamodb',
+        region_name="localhost",
+        endpoint_url="http://localhost:8000",
+        aws_access_key_id="DEFAULT_ACCESS_KEY",
+        aws_secret_access_key="DEFAULT_SECRET"
+    )
+
 def create(event, context):
     data = json.loads(event['body'])
-    if 'text' not in data:
+    if 'weight' not in data:
         logging.error("Validation Failed")
-        raise Exception("Text not found")
+        raise Exception("Weight not found")
+    if 'sub' not in data:
+        logging.error("Validation Failed")
+        raise Exception("Sub not found")
 
-    timestamp = str(time.time())
+    table = dynamodb.Table(os.environ['DynamodbTableWeight'])
 
-    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    timestamp = str(datetime.datetime.now())
 
     item = {
-        'id': str(uuid.uuid1()),
-        'text': data['text'],
-        'checked': False,
+        'cognitoUserSub': data["sub"],
+        'nextTotalingFlg': "T",
+        'weight': data["weight"],
         'createdAt': timestamp,
         'updatedAt': timestamp,
     }
