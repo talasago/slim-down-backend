bf785639031b1601fccefa44369f90deeb9e8ea0
talasago <lyricaldragons@gmail.com>
2021-08-18 17:56:53 +0900

    feat: cors対応

--- serverless/create.py
+++ serverless/create.py
@@ -41,6 +41,11 @@ def create(event, context):
 
     response = {
         "statusCode": 200,
+        'headers': {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST",
+            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        },
         "body": json.dumps(item)
     }
 
