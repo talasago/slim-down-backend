4ccd5b6a75cb0ecdb144f279e3083a9451c3f9e2
talasago <lyricaldragons@gmail.com>
2021-10-04 22:57:08 +0900

    feat: コミュニティjoin追加

---
+++ serverless/community/models/community_weight.py
@@ -0,0 +1,72 @@
+import boto3
+import os
+import datetime
+
+dynamodb = boto3.resource('dynamodb')
+if os.getenv('IS_OFFLINE') is not None or \
+   os.getenv('AWS_LAMBDA_FUNCTION_NAME') is None:
+    dynamodb = boto3.resource(
+        'dynamodb',
+        region_name="ap-northeast-1",  # localstack
+        endpoint_url="http://localhost:4566", # localstack
+        aws_access_key_id="DEFAULT_ACCESS_KEY",
+        aws_secret_access_key="DEFAULT_SECRET"
+    )
+table = dynamodb.Table(os.environ['COMMUNITY_WEIGHT'])
+
+
+class CommunityWeight:
+    def __init__(self,
+                 community_id,
+                 totaling_date,
+                 weight=None,
+                 next_totaling_flg=None,
+                 belong_sub_list=None):
+        self.community_id: str = community_id
+        self.totaling_date: str = totaling_date
+        self.weight: float = weight
+        self.next_totaling_flg: str = next_totaling_flg
+        self.belong_sub_list: list = belong_sub_list
+
+    def upsert_commu_weight(self, sub: str) -> dict:
+        timestamp = str(datetime.datetime.now())
+
+        # sublistを取得してマージする
+        res_get = table.get_item(
+            Key={
+                'communityId': self.community_id,
+                'totalingDate': self.totaling_date
+            },
+            ProjectionExpression="belongSubList"
+        )
+
+        if res_get.get('Item') is None:
+            sub_list = [sub]
+        else:
+            sub_list: list = res_get['Item']['belongSubList']
+            sub_list.append(sub)
+            sub_list = list(set(sub_list))
+
+        res_update = table.update_item(
+            Key={
+                'communityId': self.community_id,
+                'totalingDate': self.totaling_date
+            },
+            ReturnValues='UPDATED_NEW',
+            UpdateExpression='SET #flg = :flg, \
+                                  #sub_list = :sub_list, \
+                                  #time = :time',
+            ExpressionAttributeNames={
+                '#flg': 'nextTotalingFlg',
+                '#sub_list': 'belongSubList',
+                '#time': 'updatedAt'
+            },
+            ExpressionAttributeValues={
+                ':flg': 'T',
+                ':sub_list': sub_list,
+                ':time': timestamp,
+            }
+        )
+        print(res_update)
+
+        return {'massage': "コミュニティに参加しました"}
--- serverless/community/tests/test_community.py
+++ serverless/community/tests/test_community.py
@@ -1,14 +1,40 @@
+from unittest import TestCase
+from dotenv import load_dotenv
 import json
 import os
 import sys
+import boto3
+import datetime
+from boto3.session import Session
+import configparser
 
 currrent_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(currrent_path, "../"))
-os.environ['COMMUNITY_INFO'] = 'slim-down-community-info-dev'
+
+COMMUNITY_WEIGHT = 'slim-down-community-weight-local'
+os.environ['COMMUNITY_INFO'] = 'slim-down-community-info-local'
+os.environ['COMMUNITY_WEIGHT'] = COMMUNITY_WEIGHT
 import create  # noqa: E402
 import read    # noqa: E402
 import update  # noqa: E402
 import delete  # noqa: E402
+import comunity_join  # noqa: E402
+load_dotenv()
+
+session = Session(profile_name=os.getenv('AWS_PROFILE'))
+client_cip = session.client('cognito-idp')
+dynamodb = boto3.resource(
+    'dynamodb',
+    region_name="ap-northeast-1",  # localstack用
+    endpoint_url="http://localhost:4566",
+    aws_access_key_id="DEFAULT_ACCESS_KEY",
+    aws_secret_access_key="DEFAULT_SECRET"
+)
+community_weight = dynamodb.Table(COMMUNITY_WEIGHT)
+
+config_ini = configparser.ConfigParser()
+path_ini = os.path.join(currrent_path, "tests-data/test_community.ini")
+config_ini.read(path_ini, encoding='utf-8')
 
 
 def test_create_200():
@@ -81,3 +107,56 @@ def test_delete_200():
     res = delete.delete(event, '')
 
     assert res['statusCode'] == 200
+
+
+def test_community_join_200():
+    res_auth = client_cip.initiate_auth(
+        AuthFlow="USER_PASSWORD_AUTH",
+        ClientId=os.getenv("cognitoClientId"),
+        AuthParameters={
+            'USERNAME': config_ini['auth']['USERNAME'],
+            'PASSWORD': config_ini['auth']['PASSWORD']
+        }
+    )
+
+    access_token = res_auth['AuthenticationResult']["AccessToken"]
+
+    body = {
+        'communityId': 'test_commu2',
+        'sub': '76e6f480-5f0a-4863-97f8-aff844774a5c'
+    }
+
+    event = {
+        'headers': {'Authorization': {
+            'AccessToken': access_token
+        }},
+        'body': json.dumps(body)
+    }
+
+    res = comunity_join.community_join(event, '')
+    assert res['statusCode'] == 200
+
+    today = datetime.datetime.now()
+    totaling_date = today.strftime('%Y%m%d')
+    item: dict = community_weight.get_item(
+        Key={
+            'communityId': body['communityId'],
+            'totalingDate': totaling_date
+        },
+        ProjectionExpression="communityId,   \
+                              totalingDate, \
+                              nextTotalingFlg, \
+                              belongSubList"
+    )
+    actual_sub_list = item['Item']['belongSubList']
+    assert body['sub'] in actual_sub_list
+
+    user = client_cip.get_user(
+        AccessToken=access_token
+    )
+    attrs = user['UserAttributes']
+    for attr in attrs:
+        if attr['Name'] == 'custom:community_id':
+            exsist_flg = True
+            assert attr['Value'] == body['communityId']
+    assert exsist_flg
